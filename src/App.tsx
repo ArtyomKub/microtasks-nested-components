a5c6e7c9b95a0896f5ba3b368eee7b2f3ccc93ad
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-16 10:45:23 +0200

    Create a new component Header.tsx and connect in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Header} from './site/Header';
 
 function App() {
   return (
     <div className="App">
       hello world
+        <Header/>
     </div>
   );
 }
